4bac97cd8d5d74de8b39cd5cdcece1ab130d4e07
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:53 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "monitor_autoscale_settings" {
-  description = "All monitor_autoscale_setting resources"
-  value       = azurerm_monitor_autoscale_setting.monitor_autoscale_settings
-}
 output "monitor_autoscale_settings_enabled" {
-  description = "List of enabled values across all monitor_autoscale_settings"
-  value       = [for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : v.enabled]
+  description = "Map of enabled values across all monitor_autoscale_settings, keyed the same as var.monitor_autoscale_settings"
+  value       = { for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : k => v.enabled }
 }
 output "monitor_autoscale_settings_location" {
-  description = "List of location values across all monitor_autoscale_settings"
-  value       = [for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : v.location]
+  description = "Map of location values across all monitor_autoscale_settings, keyed the same as var.monitor_autoscale_settings"
+  value       = { for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : k => v.location }
 }
 output "monitor_autoscale_settings_name" {
-  description = "List of name values across all monitor_autoscale_settings"
-  value       = [for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : v.name]
+  description = "Map of name values across all monitor_autoscale_settings, keyed the same as var.monitor_autoscale_settings"
+  value       = { for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : k => v.name }
 }
 output "monitor_autoscale_settings_notification" {
-  description = "List of notification values across all monitor_autoscale_settings"
-  value       = [for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : v.notification]
+  description = "Map of notification values across all monitor_autoscale_settings, keyed the same as var.monitor_autoscale_settings"
+  value       = { for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : k => v.notification }
 }
 output "monitor_autoscale_settings_predictive" {
-  description = "List of predictive values across all monitor_autoscale_settings"
-  value       = [for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : v.predictive]
+  description = "Map of predictive values across all monitor_autoscale_settings, keyed the same as var.monitor_autoscale_settings"
+  value       = { for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : k => v.predictive }
 }
 output "monitor_autoscale_settings_profile" {
-  description = "List of profile values across all monitor_autoscale_settings"
-  value       = [for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : v.profile]
+  description = "Map of profile values across all monitor_autoscale_settings, keyed the same as var.monitor_autoscale_settings"
+  value       = { for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : k => v.profile }
 }
 output "monitor_autoscale_settings_resource_group_name" {
-  description = "List of resource_group_name values across all monitor_autoscale_settings"
-  value       = [for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : v.resource_group_name]
+  description = "Map of resource_group_name values across all monitor_autoscale_settings, keyed the same as var.monitor_autoscale_settings"
+  value       = { for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : k => v.resource_group_name }
 }
 output "monitor_autoscale_settings_tags" {
-  description = "List of tags values across all monitor_autoscale_settings"
-  value       = [for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : v.tags]
+  description = "Map of tags values across all monitor_autoscale_settings, keyed the same as var.monitor_autoscale_settings"
+  value       = { for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : k => v.tags }
 }
 output "monitor_autoscale_settings_target_resource_id" {
-  description = "List of target_resource_id values across all monitor_autoscale_settings"
-  value       = [for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : v.target_resource_id]
+  description = "Map of target_resource_id values across all monitor_autoscale_settings, keyed the same as var.monitor_autoscale_settings"
+  value       = { for k, v in azurerm_monitor_autoscale_setting.monitor_autoscale_settings : k => v.target_resource_id }
 }
 
